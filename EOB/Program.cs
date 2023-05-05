efba28741c73b0beaac2fbb9f8eb7e720ae04b8a
Alanlin15 <116563746+Alanlin15@users.noreply.github.com>
2023-05-05 09:59:49 +0200

    Add files via upload

---
+++ EOB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EOB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EOB/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOB
+{
+    internal class User
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        protected string password { get; set; }
+        public string email { get; set; }
+    }
+}
